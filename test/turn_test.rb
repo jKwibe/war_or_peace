966573ad563825ffa401f132367c7c113463c3dc
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-10 18:51:23 -0600

    Add more test for clear output

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,250 +1,245 @@
-#turn type :basic
+
 require "minitest/autorun"
 require "minitest/pride"
- require './lib/card'
- require './lib/deck'
- require './lib/player'
- require './lib/turn'
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+require "./lib/turn"
 
- class TurnTest < Minitest::Test
-   def test_it_exists
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+class TurnTest < Minitest::Test
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+  def test_it_has_players
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
 
-     turn = Turn.new(player1, player2)
+    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck2
 
-     assert_instance_of Turn, turn
-   end
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Khaleese', deck2)
 
-   def test_it_has_players
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+    turn = Turn.new(player1, player2)
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    assert_instance_of Turn, turn
+  end
 
-     turn = Turn.new(player1, player2)
+  def test_it_has_a_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-     assert_equal 'Megan', turn.player1.name
-     assert_equal 'Aurora', turn.player2.name
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
 
-     assert_instance_of Deck, turn.player1.deck
-   end
-   def test_it_has_spoils_of_war
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck2
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Khaleese', deck2)
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    # require "pry"
+    # binding.pry
 
-     turn = Turn.new(player1, player2)
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
 
-     assert_equal [], turn.spoils_of_war
-   end
-   def test_it_has_a_basic_type
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+    turn = Turn.new(player1, player2)
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+    assert_instance_of Turn, turn
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    assert_equal :basic , turn.type
 
-     turn = Turn.new(player1, player2)
 
-     assert_equal :basic , turn.type
-   end
-   def test_it_has_winner_at_basic_type
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+  end
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+  def test_it_has_winner
+    card1 = Card.new(:heart, 'Jack', 11 )
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
 
-     turn = Turn.new(player1, player2)
+    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck2
 
-     assert_equal "Megan", turn.winner.name
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Khaleese', deck2)
 
-   end
-   def test_it_can_pile_cards
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-     turn = Turn.new(player1, player2)
+    assert_instance_of Turn, turn
 
-     turn.pile_cards
+    assert_equal :basic , turn.type
 
-     assert_equal 2, turn.spoils_of_war.size
 
-   end
-   def test_it_has_a_war_type
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, 'Queen', 12)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
+    assert_equal "Clarisa",turn.winner.name
 
-      deck1 = Deck.new([card1, card2, card5, card8])
 
-      deck2 = Deck.new([card4, card3, card6, card7])
+  end
 
-      player1 = Player.new("Megan", deck1)
+  def test_it_can_pile_cards
+    card1 = Card.new(:heart, 'Jack', 11 )
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-      player2 = Player.new("Aurora", deck2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
 
-      turn = Turn.new(player1, player2)
+    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck2
 
-      assert_equal :war , turn.type
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Khaleese', deck2)
 
-      turn.pile_cards
-      assert_equal 6 , turn.spoils_of_war.size
 
-   end
-   def test_it_has_a_mutually_assured_destruction_type
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, '8', 8)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
 
-      deck1 = Deck.new([card1, card2, card5, card8])
+    turn = Turn.new(player1, player2)
 
-      deck2 = Deck.new([card4, card3, card6, card7])
+    assert_instance_of Turn, turn
 
-      player1 = Player.new("Megan", deck1)
+    assert_equal :basic , turn.type
 
-      player2 = Player.new("Aurora", deck2)
+    turn.pile_cards
 
-      turn = Turn.new(player1, player2)
+    assert_equal 2, turn.spoils_of_war.size
 
-      assert_equal :mutually_assured_destruction , turn.type
 
-      turn.pile_cards
-      assert_equal 1 , turn.player1.deck.cards.size
-      assert_equal 1 , turn.player2.deck.cards.size
-      assert_equal [] , turn.spoils_of_war
-   end
-   def test_it_has_winner_at_mutually_assured_destruction_type
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, '8', 8)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
 
-     deck1 = Deck.new([card1, card2, card5, card8])
+  end
 
-     deck2 = Deck.new([card4, card3, card6, card7])
+  def test_it_can_award_points
+    card1 = Card.new(:heart, 'Jack', 11 )
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-     player1 = Player.new("Megan", deck1)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
 
-     player2 = Player.new("Aurora", deck2)
+    assert_instance_of Deck, deck1
+    assert_instance_of Deck, deck2
 
-     turn = Turn.new(player1, player2)
+    player1 = Player.new('Clarisa', deck1)
+    player2 = Player.new('Khaleese', deck2)
 
-     assert_equal "No winner", turn.winner
-   end
 
-   def test_it_award_spoils_basic
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     card3 = Card.new(:heart, '9', 9)
-     card4 = Card.new(:diamond, 'Jack', 11)
-     card5 = Card.new(:heart, '8', 8)
-     card6 = Card.new(:diamond, 'Queen', 12)
-     card7 = Card.new(:heart, '3', 3)
-     card8 = Card.new(:diamond, '2', 2)
+    assert_instance_of Player, player1
+    assert_instance_of Player, player2
 
-     deck1 = Deck.new([card1, card2, card5, card8])
-     deck2 = Deck.new([card3, card4, card6, card7])
+    turn = Turn.new(player1, player2)
 
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
+    assert_instance_of Turn, turn
 
-     turn = Turn.new(player1, player2)
+    assert_equal :basic , turn.type
+    # require "pry"
+    # binding.pry
+    turn.pile_cards
 
-     winner = turn.winner
-     turn.pile_cards
+    winner = turn.winner
+    turn.award_spoils(winner)
+    assert_equal 5, winner.deck.cards.size
+  end
 
-     assert_equal 2, turn.spoils_of_war.size
+  def test_it_has_a_type_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
 
-     turn.award_spoils(winner)
-     
-     assert_equal 5 , winner.deck.cards.size
-     assert_equal [], turn.spoils_of_war
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
 
-   end
- end
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
 
- # pry(main)> turn.award_spoils(winner)
- #
- # pry(main)> player1.deck
- # #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>, #<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3ed98d9b8...>]>
- # pry(main)> player2.deck
- # #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
+    turn = Turn.new(player1, player2)
+    # require "pry"
+    # binding.pry
+
+    assert_equal :war , turn.type
+
+    turn.pile_cards
+
+    winner = turn.winner
+    turn.award_spoils(winner)
+
+    assert_equal 7, winner.deck.cards.size
+    assert_equal 1 , player1.deck.cards.size
+  end
+
+  def test_it_has_a_type_mutually_assured_destruction
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction , turn.type
+
+    assert_equal "No winner", turn.winner
+
+    turn.pile_cards
+
+      require "pry"
+      binding.pry
+    assert_equal 1 , turn.player2.deck.cards.size
+    assert_equal 1 , turn.player1.deck.cards.size
+
+    assert_equal :basic , turn.type
+    assert_equal "Aurora", turn.winner.name
+  end
+
+end
