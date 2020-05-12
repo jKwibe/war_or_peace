ffc873e1765dfb25ecf49e7b54276a8622b849f5
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-12 15:56:01 -0600

    Refactor the if else statements

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,51 +11,42 @@ class Turn
        player2.deck.rank_of_card_at(0) &&
        player1.deck.rank_of_card_at(2) ==
        player2.deck.rank_of_card_at(2)
-          :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) !=
-          player2.deck.rank_of_card_at(0)
-          :basic
-    elsif player1.deck.rank_of_card_at(2)!=
-          player2.deck.rank_of_card_at(2)
-          :war
+        return :mutually_assured_destruction
+    end
+    if player1.deck.rank_of_card_at(0) !=
+       player2.deck.rank_of_card_at(0)
+        return :basic
+    end
+    if player1.deck.rank_of_card_at(2)!=
+       player2.deck.rank_of_card_at(2)
+        return :war
     end
   end
+
   def winner
-
-    case self.type
-    when :basic
-      if player1.deck.rank_of_card_at(0) >
-        player2.deck.rank_of_card_at(0)
-        player1
-      else
-        player2
-      end
-    when :war
-
-      if player1.deck.rank_of_card_at(2) >
-        player2.deck.rank_of_card_at(2)
-        player1
-      else
-        player2
-      end
-    else
-      "No winner"
+    if type == :basic
+      return [player1 , player2].max_by{|player| player.deck.rank_of_card_at(0)}
     end
-
+    if type == :war
+      return [player1 , player2].max_by{|player| player.deck.rank_of_card_at(2)}
+    end
+    if type == :mutually_assured_destruction
+      return "No winner"
+    end
   end
 
   def pile_cards
-
     case self.type
-    when :basic
-      @spoils_of_war.concat(player1.deck.remove_card(1), player2.deck.remove_card(1)) 
-    when :war
-      @spoils_of_war.concat(player1.deck.remove_card(3), player2.deck.remove_card(3))
-    else
-      player1.deck.remove_card(3)
-      player2.deck.remove_card(3)
-    end
+      when :basic
+        @spoils_of_war.concat(player1.deck.remove_card(1), player2.deck.remove_card(1))
+      when :war
+        @spoils_of_war.concat(player1.deck.remove_card(3), player2.deck.remove_card(3))
+      else
+        player1.deck.remove_card(3)
+        player2.deck.remove_card(3)
+      end
   end
+  
   def award_spoils(champ)
       (champ.deck.cards.concat(@spoils_of_war))
       @spoils_of_war = []
